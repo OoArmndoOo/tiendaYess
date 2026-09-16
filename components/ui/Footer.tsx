04754039c3eb5aa16afec99e636f5bea745da45a
ArmndoAR <ooarmndooo@gmail.com>
2026-09-15 23:23:01 -0400

    derechos

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer() {
 
         {/* Copyright */}
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} MiireTech. Todos los derechos reservados.</p>
+          <p>&copy; {new Date().getFullYear()} ArmndoDev. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
